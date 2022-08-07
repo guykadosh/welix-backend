6c134d68a8137a891d09b45634bb90ba5a189ac9
Guy Kadosh <99971417+guykadosh@users.noreply.github.com>
2022-08-07 08:54:08 +0300

    deleting config

--- config/dev.js
+++
@@ -1,5 +0,0 @@
-module.exports = {
-  dbURL:
-    'mongodb+srv://yuvalsh:123@yuval.ojzri.mongodb.net/?retryWrites=true&w=majority',
-  // dbURL: 'mongodb://localhost:27017',
-}
--- config/prod.js
+++
@@ -1,5 +0,0 @@
-module.exports = {
-  dbURL:
-    'mongodb+srv://yuvalsh:123@yuval.ojzri.mongodb.net/?retryWrites=true&w=majority'
-    //'mongodb+srv://guyk:gG6887422@cluster0.455xb.mongodb.net/?retryWrites=true&w=majority',
-}
