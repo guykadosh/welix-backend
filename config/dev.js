8067d06b50f16bdcbaf99c5301d93cb9b1ea24bb
Guy Kadosh <guyk50@gmail.com>
2022-10-26 12:44:31 +0300

    added config

---
+++ config/dev.js
@@ -0,0 +1,4 @@
+module.exports = {
+  dbURL: `mongodb+srv://yuvalsh:${process.env.DB_KEY}@yuval.ojzri.mongodb.net/?retryWrites=true&w=majority`,
+  // dbURL: 'mongodb://localhost:27017',
+}
---
+++ config/prod.js
@@ -0,0 +1,3 @@
+module.exports = {
+  dbURL: `mongodb+srv://yuvalsh:${process.env.DB_KEY}@yuval.ojzri.mongodb.net/?retryWrites=true&w=majority`,
+}
